b65c622dd5c459eec1c7e893605d2626e3f8624e
cmderobertis <cmderobertis@gmail.com>
2022-08-09 21:55:14 -0700

    updated README.md and '/' route.

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, session, request, redirect, url_for
-from database_name import SomeClass
+from database_name import ClassName, And, Any, Other, Table, Classes, You, Have
 
 app = Flask(__name__)
-app.secret_key = 'there is a fire in the building'
+app.secret_key = 'I suppose changing this is not absolutely necessary, but do it anyway.'
 
 
 @app.route('/')
 def index():
-    friends = SomeClass.get_all()
-    return render_template('index.html', friends=friends)
+    friends = ClassName.get_all()
+    if friends[0]:
+        print("First item:", friends[0])
+    return render_template('index.html', friends=friends, page_title='My Flask App')
 
 
 if __name__ == '__main__':
